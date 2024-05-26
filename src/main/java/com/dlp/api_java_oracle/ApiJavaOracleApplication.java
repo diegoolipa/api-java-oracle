9c618be25ae9e99afada2908834c3f61668b8bde
diego.lipa <diego.lipa@lim.upeu.edu.pe>
2024-05-25 22:36:53 -0500

    coneccion oracle

---
+++ src/main/java/com/dlp/api_java_oracle/ApiJavaOracleApplication.java
@@ -0,0 +1,13 @@
+package com.dlp.api_java_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlp/api_java_oracle/ApiJavaOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlp.api_java_oracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
